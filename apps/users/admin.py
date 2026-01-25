eba4d50d938408060cc0398efbcb60aa0564271c
unknown <rachitkumarsingh144@gmail.com>
2026-01-25 16:58:14 +0530

    Add magic fix link

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -9,14 +9,17 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     """Admin for CustomUser model."""
-    list_display = ['username', 'email', 'is_tenant_admin', 'repurposes_used_this_month', 'is_staff']
-    list_filter = ['is_tenant_admin', 'is_staff', 'is_active']
-    search_fields = ['username', 'email', 'first_name', 'last_name']
+    list_display = ['username', 'email', 'subscription_tier', 'repurposes_used_this_month', 'is_staff', 'is_active']
+    list_filter = ['subscription_tier', 'is_tenant_admin', 'is_staff', 'is_active']
+    search_fields = ['username', 'email', 'first_name', 'last_name', 'stripe_customer_id']
     ordering = ['-date_joined']
     
     fieldsets = UserAdmin.fieldsets + (
         ('Tenant Role', {
             'fields': ('is_tenant_admin',)
+        }),
+        ('Subscription', {
+            'fields': ('subscription_tier', 'stripe_customer_id', 'stripe_subscription_id')
         }),
         ('Usage', {
             'fields': ('repurposes_used_this_month', 'usage_reset_date')
