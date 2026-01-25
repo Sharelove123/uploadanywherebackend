558fb343d6d66591572e9c37b263fbe8068b5d73
unknown <rachitkumarsingh144@gmail.com>
2026-01-25 15:39:54 +0530

    Add FACEBOOK to RepurposedPost.Platform enum

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+# Auto-create superuser from environment variables (if not exists)
+python manage.py createsuperuser --noinput || true
+
 # NOTE: Database migrations should NOT run in the build step on Render/Heroku
 # because the database is often not accessible during the build phase.
 # We will run them in the Start Command or entrypoint.
